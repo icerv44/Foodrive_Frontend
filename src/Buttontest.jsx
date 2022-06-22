ac660ab93fcb5eafc3a480d1f722e7a79d12dca3
icerv44 <icerv44@hotmail.com>
2022-06-22 17:05:29 +0700

    Button

--- src/Buttontest.jsx
+++ src/Buttontest.jsx
@@ -2,23 +2,25 @@ import * as React from "react";
 
 import { makeStyles } from "@mui/styles";
 import Button from "@mui/material/Button";
-const useStyles = makeStyles({
-  root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-  },
-});
+import Typography from "@mui/material/Typography";
 
 function Buttontest() {
-  const calsses = useStyles();
   return (
     <>
-      <Button className={calsses.root}>Styled with Hook API</Button>
+      <Button
+        //   className={calsses.root}
+        variant="contained"
+        sx={{
+          background:
+            "linear-gradient(98.81deg, #53E88B -0.82%, #15BE77 101.53%)",
+          color: "white",
+          height: "57px",
+          width: "175px",
+          borderRadius: "15px",
+        }}
+      >
+        Create Account
+      </Button>
     </>
   );
 }
