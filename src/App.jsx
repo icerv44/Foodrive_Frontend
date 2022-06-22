e3b32b6ea0ee43b3d835db65f7e8512d813d894f
icerv44 <icerv44@hotmail.com>
2022-06-22 16:08:06 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+
+import Buttontest from "./Buttontest";
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Buttontest />
+    </div>
+  );
+}
+
+export default App;
