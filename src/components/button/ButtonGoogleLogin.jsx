e4c81b36ef3be1d4e5a7d024fb8526354e53a10c
icerv44 <icerv44@hotmail.com>
2022-06-23 21:11:01 +0700

    First

--- src/components/button/ButtonGoogleLogin.jsx
+++ src/components/button/ButtonGoogleLogin.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import GoogleIcon from "@mui/icons-material/Google";
+import Button from "@mui/material/Bzutton";
+
 import { FcGoogle } from "react-icons/fc";
 
 function ButtonGoogleLogin() {
--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -1,10 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import ButtonGoogleLogin from "../components/button/ButtonGoogleLogin";
+import CardHome from "../components/card/CardHome";
 import LoginPage from "../pages/LoginPage";
 
 function Router() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/card"
+        element={
+          <CardHome Name="Pizza Hut (พิซซ่าฮัท) - กทม." Time="12" Distance="" />
+        }
+      />
+      <Route path="/google" element={<ButtonGoogleLogin />} />
     </Routes>
   );
 }
