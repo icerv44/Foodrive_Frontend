27cd21505f75ce6ebb8b0d20563dc0414d2fb6b3
Rose <rozurosemary@gmail.com>
2022-06-28 11:30:08 +0700

    register only customer

--- src/slices/registerSlice.js
+++ src/slices/registerSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { useLocation } from "react-router-dom";
 import axios from "../config/axios";
 import { setAccessToken } from "../services/localstorage";
 
