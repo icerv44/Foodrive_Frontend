9e1079b003ee40e42199dcc9c9baf56187ca31b6
theme <theme.2541@gmail.com>
2022-06-28 12:42:08 +0700

    deleted env.js file tracking

--- src/pages/driver/HomePageDriver.jsx
+++ src/pages/driver/HomePageDriver.jsx
@@ -1,6 +1,10 @@
 import { Box } from "@mui/joy";
 import React from "react";
 import HomeContainerDriver from "../../role/driver/home/HomeContainerDriver";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+console.log(window.process.env.REACT_APP_GOOGLE_MAP_KEY);
 
 function HomePageDriver() {
   return (
