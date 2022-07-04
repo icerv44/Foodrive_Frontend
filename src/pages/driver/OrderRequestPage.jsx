66c8bfce50214042fb77e86a3a6e661fa27a1744
icerv44 <icerv44@hotmail.com>
2022-07-04 22:41:45 +0700

    order request & modal

--- src/pages/driver/OrderRequestPage.jsx
+++ src/pages/driver/OrderRequestPage.jsx
@@ -3,10 +3,11 @@ import { Box, Typography } from "@mui/joy";
 import axios from "../../config/axios";
 import { useSelector } from "react-redux";
 import React, { useEffect, useState } from "react";
-import ButtonBack from "../../components/button/ButtonBack";
 import ButtonBackNewPlus from "../../components/button/ButtonBackNewPlus";
 // import ButtonBacknew from ""
 import CardOrderReq from "../../components/card/CardOrderReq";
+import Modal from "react-modal";
+import Card from "@mui/joy/Card";
 
 function OrderRequestPage() {
   const { latitude, longitude } = useSelector((state) => state.user.info);
@@ -34,45 +35,6 @@ function OrderRequestPage() {
     }
   };
 
-  const orderRequest = [
-    {
-      restaurantName: "Starbucks Coffee",
-      distance: 19,
-      driverIncome: 20,
-      orderList: [{ menuTitle: "Late", pieces: 2 }],
-    },
-    {
-      restaurantName: "Starbucks Coffee",
-      distance: 19,
-      driverIncome: 20,
-      orderList: [
-        { menuTitle: "Late", pieces: 2 },
-        { menuTitle: "Green Tea", pieces: 1 },
-      ],
-    },
-    {
-      restaurantName: "Starbucks Coffee",
-      distance: 19,
-      driverIncome: 20,
-      orderList: [
-        { menuTitle: "Late", pieces: 2 },
-        { menuTitle: "Green Tea", pieces: 1 },
-        { menuTitle: "Milk Tea", pieces: 3 },
-      ],
-    },
-    {
-      restaurantName: "Starbucks Coffee",
-      distance: 19,
-      driverIncome: 20,
-      orderList: [
-        { menuTitle: "Late", pieces: 2 },
-        { menuTitle: "Green Tea", pieces: 1 },
-        { menuTitle: "Milk Tea", pieces: 3 },
-        { menuTitle: "Milk Tea", pieces: 3 },
-        { menuTitle: "Milk Tea", pieces: 3 },
-      ],
-    },
-  ];
   const clickOrderAccepted = async (id) => {
     const resOrder = await axios.post(`driver/deliveringStatus/${id}`);
     console.log("Click : ", resOrder);
@@ -105,6 +67,58 @@ function OrderRequestPage() {
                 driverIncome={el.deliveryFee}
                 orderList={el.OrderMenus}
               />
+              <Modal
+                style={{
+                  overlay: { backgroundColor: "rgba(0,0,0,0.5)" },
+                  content: {
+                    borderRadius: "18px",
+                    boxShadow: "12px 26px 50px rgba(90, 108, 234, 0.07)",
+                    height: "26vh",
+                    top: "33%",
+                  },
+                }}
+                isOpen={isOpen}
+                onRequestClose={() => setIsOpen(false)}
+              >
+                <div>
+                  <Card
+                    className="shadow-lg shadow-blue-100 rounded-lg my-3 "
+                    sx={{
+                      width: "320px",
+                      background: "#fafdff",
+                      "&:hover": {
+                        boxShadow: "md",
+                        borderColor: "neutral.outlinedHoverBorder",
+                      },
+                    }}
+                  >
+                    <CardContent className="flex justify-between items-center">
+                      <Box className="flex flex-col  gap-2">
+                        {/* Distance */}
+                        <Typography
+                          className="pl-10 text-20 font-bold text-gray"
+                          fontSize={14}
+                          // fontWeight="bold"
+                        >
+                          far from you {distance} km.
+                        </Typography>
+
+                        {/* Restaurant name */}
+                        <Box className="flex items-center">
+                          <MdOutlineLocationOn className="text-green text-2xl" />
+                          <Typography
+                            //   className=" text-20 font-bold"
+                            fontSize={27}
+                            fontWeight="bold"
+                          >
+                            {cutRestaurantName(restaurantName)}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </div>
+              </Modal>
             </Box>
           </>
         ))}
