4220bf1c16ad95091240207104069d7791899224
Rose <rozurosemary@gmail.com>
2022-07-04 16:58:53 +0700

    remove modal menu from cart

--- src/role/customer/order/CardOrder.jsx
+++ src/role/customer/order/CardOrder.jsx
@@ -3,8 +3,11 @@ import IconButton from "@mui/joy/IconButton";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { useState } from "react";
 import { Chip } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
 import { useCustomer } from "../../../contexts/CustomerContext";
 import { useParams } from "react-router-dom";
+import ModalVertical from "../../../components/ui/ModalVertical";
+import Modal from "react-modal";
 
 function CardOrder({
   id,
@@ -17,6 +20,7 @@ function CardOrder({
   const { deleteMenu, getCartById } = useCustomer();
   const [count, setCount] = useState(1);
   const { cartId } = useParams();
+  Modal.setAppElement("#root");
 
   const handleClickIncreaseAmount = () => {
     if (count <= 8) {
@@ -39,48 +43,48 @@ function CardOrder({
   };
 
   return (
-    <Box
-      sx={{
-        borderRadius: "28px",
-        width: "348px",
-        // height: "100px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        px: "14px",
-        py: "14px",
-        boxShadow: "12px 26px 50px rgba(90, 108, 234, 0.07)",
-      }}
-    >
-      <img src={src} className="w-[66px]" alt="" />
-      <Box sx={{ flexGrow: "1" }} className="flex flex-col px-4">
-        <Box className="flex justify-end"></Box>
+    <Box>
+      <Box
+        sx={{
+          borderRadius: "28px",
+          width: "348px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          px: "14px",
+          py: "14px",
+          boxShadow: "12px 26px 50px rgba(90, 108, 234, 0.07)",
+        }}
+      >
+        <img src={src} className="w-[66px]" alt="" />
+        <Box sx={{ flexGrow: "1" }} className="flex flex-col px-4">
+          <Box className="flex justify-end"></Box>
 
-        <Box>
-          <Typography sx={{ fontWeight: 700 }}>{foodName}</Typography>
-          <Typography sx={{ color: "#3B3B3B" }}>
-            {orderMenuOptionGroups.length >= 0 &&
-              orderMenuOptionGroups.map(
-                (el) => el.name + " : " + el.options.map((el) => el.name)
-              )}
+          <Box>
+            <Typography sx={{ fontWeight: 700 }}>{foodName}</Typography>
+            <Typography sx={{ color: "#3B3B3B" }}>
+              {orderMenuOptionGroups.length >= 0 &&
+                orderMenuOptionGroups.map(
+                  (el) => el.name + " : " + el.options.map((el) => el.name)
+                )}
+            </Typography>
+          </Box>
+          <Typography
+            sx={{ fontWeight: 800, color: "#15BE77", fontSize: "20px" }}
+          >
+            {price} ฿
           </Typography>
         </Box>
-        <Typography
-          sx={{ fontWeight: 800, color: "#15BE77", fontSize: "20px" }}
-        >
-          {price} ฿
-        </Typography>
-      </Box>
 
-      <Box onClick={handleRemove}>
-        <Chip
-          variant="outlined"
-          color="danger"
-          size="sm"
-          sx={{ pointerEvents: "none" }}
+        <ModalVertical
+          onAction={handleRemove}
+          id={id}
+          title={"REMOVE MENU"}
+          content={"This menu will be remove from cart."}
+          btnName={"REMOVE"}
         >
           Remove
-        </Chip>
+        </ModalVertical>
       </Box>
     </Box>
   );
